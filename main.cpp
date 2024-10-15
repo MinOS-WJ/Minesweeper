11e58d3acd26ac7481882fb790dd0f61454818c2
MinOS-WJ <13871575863@139.com>
2024-10-15 19:53:58 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,81 +1,106 @@
 #include <iostream>
 #include <cstdlib>
+//#include <stdlib.h>
 #include <ctime>
 using namespace std;
 
-
 int x=1,y=1;
-int map1[1000][1000] = {0};
-int map2[100][100] = {0};
-int map3[10][10] = {0};
+int map[100][100];//Use 100X100 to save memory
+int mine_map[100][100];
+short int inputCheck=0;//Use short int to save memory
 
 //void creatMap(int x_axis,int y_axis) BY MinOS
-void creatMap(int x_axis,int y_axis)
-{
-
-    for (int i = 0; i < y_axis; ++i) 
-    {
-        for (int j = 0; j <x_axis; ++j) 
-        {
-            map3[i][j]=1;
+void creatMap(int x_axis,int y_axis){
+    for (int i = 0; i < y_axis; ++i) {
+        for (int j = 0; j <x_axis; ++j) {
+            map[i][j]=-1;
         }
     }
 }
 
 //void printMap(int x_axis,int y_axis) BY MinOS
-void printMap(int x_axis,int y_axis)
-{
+void printMap(int x_axis,int y_axis){
     printf("y\\x");
-    for (int i = 0; i < x_axis; ++i) 
-    {
+    for (int i = 0; i < x_axis; ++i) {
         printf("%3d",i);
     }
     printf("\n");
-    for (int i = 0; i < y_axis; ++i)
-    {
+    for (int i = 0; i < y_axis; ++i) {
         printf("%3d",i);
-        for (int j = 0; j <x_axis ; ++j) 
-        {
+        for (int j = 0; j <x_axis ; ++j) {
             char outChar;
-            if(map3[i][j]==1)
-            {
-                outChar='*';
+            if(map[i][j]==-1){//Unknown
+                outChar='?';
             }
-            else
-            {
+            else if(map[i][j]==0){
+                outChar='0';
+            }
+            else if(map[i][j]==1){
+                outChar='1';
+            }
+            else if(map[i][j]==2){
+                outChar='2';
+            }
+            else if(map[i][j]==3){
+                outChar='3';
+            }
+            else if(map[i][j]==4){
+                outChar='4';
+            }
+            else if(map[i][j]==5){
+                outChar='5';
+            }
+            else if(map[i][j]==6){
+                outChar='6';
+            }
+            else if(map[i][j]==7){
+                outChar='7';
+            }
+            else if(map[i][j]==8){
+                outChar='8';
+            }
+            else if(map[i][j]==9){
+                outChar='9';
+            }
+            /*
+            else if(map[i][j]==?){
+                outChar='?';
+            }
+             */
+            else{
                 outChar=' ';
             }
             printf("  %c",outChar);
         }
         printf("\n");
-    }//这个函数什么意思
-}
-void MineApper(int kick)//by lawliet
-{
-    int mine = 0 ;
-    int num = 0;//还需要重新赋值：显示周围雷数
-    if (kick == mine)
-    {
-        cout << "游戏结束" <<endl;
     }
-    else 
-    {
-        cout << num <<endl;
-    } 
-
 }
 
 
-int main() 
-{
-    srand (time(NULL));//要到哪里用
-    int kick = 0;//点击数
-    int *p_one = map1[10];
-    int *p_two = map2[100];
-    int *p_three = map3[1000];
-    std::cout << "Hello, World!" << std::endl;
 
-    scanf("%d %d",&x,&y);//为什么不用宏函数
+void putMine(){
+    int a,b;
+    srand((unsigned)time(NULL));
+    a = rand() % x ;
+    b = rand() % y ;
+    //printf("%d %d",a,b);
+    mine_map[b][a]=1;
+}
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    //inputCheck BY MinOS
+    while(inputCheck==0){
+        cin >> x >> y ;//Accept Suggestion
+        if(x>25||y>25){
+            printf("over!\n");
+            inputCheck = 0;
+        }
+        else{
+            inputCheck = 1;
+        }
+    }
 
     creatMap(x,y);
     printMap(x,y);
@@ -84,3 +109,4 @@ int main()
     getchar();
     return 0;
 }
+
