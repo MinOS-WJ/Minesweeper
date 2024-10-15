e4514842f49b7544060e39283e79a3664c8fd916
MinOS-WJ <13871575863@139.com>
2024-10-15 20:10:58 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@
 #include <ctime>
 using namespace std;
 
-int x=1,y=1;
-int map[100][100];//Use 100X100 to save memory
-int mine_map[100][100];
+int x=1,y=1,n=0;
+int map[50][50];//Use 50X50 to save memory
+int mine_map[50][50];
 short int inputCheck=0;//Use short int to save memory
 
 //void creatMap(int x_axis,int y_axis) BY MinOS
 void creatMap(int x_axis,int y_axis){
-    for (int i = 0; i < y_axis; ++i) {
-        for (int j = 0; j <x_axis; ++j) {
+    for (int i = 1; i <= y_axis; ++i) {
+        for (int j = 1; j <=x_axis; ++j) {
             map[i][j]=-1;
         }
     }
@@ -25,9 +25,9 @@ void printMap(int x_axis,int y_axis){
         printf("%3d",i);
     }
     printf("\n");
-    for (int i = 0; i < y_axis; ++i) {
+    for (int i = 1; i <= y_axis; ++i) {
         printf("%3d",i);
-        for (int j = 0; j <x_axis ; ++j) {
+        for (int j = 1; j <= x_axis ; ++j) {
             char outChar;
             if(map[i][j]==-1){//Unknown
                 outChar='?';
@@ -80,20 +80,25 @@ void printMap(int x_axis,int y_axis){
 
 void putMine(){
     int a,b;
-    srand((unsigned)time(NULL));
-    a = rand() % x ;
-    b = rand() % y ;
-    //printf("%d %d",a,b);
-    mine_map[b][a]=1;
+    for (int i = 0; i < n; ++i) {
+        srand((unsigned)time(NULL));
+        a = rand() % x ;
+        b = rand() % y ;
+        //printf("%d %d",a,b);
+        mine_map[b][a]=1;
+    }
 }
+
+
+
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     //inputCheck BY MinOS
     while(inputCheck==0){
-        cin >> x >> y ;//Accept Suggestion
-        if(x>25||y>25){
+        cin >> x >> y >> n ;//Accept Suggestion
+        if(x>25||y>25||n>x*y*0.25){
             printf("over!\n");
             inputCheck = 0;
         }
@@ -109,4 +114,3 @@ int main() {
     getchar();
     return 0;
 }
-
