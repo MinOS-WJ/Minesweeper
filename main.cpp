5febf1eb7f57016908fb7a215ae9fdcf3e32a7b0
Law-Liets <3503453585@qq.com>
2024-10-15 13:00:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,46 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
 
 int x=1,y=1;
 int map[1000][1000];
 
 //void creatMap(int x_axis,int y_axis) BY MinOS
-void creatMap(int x_axis,int y_axis){
+void creatMap(int x_axis,int y_axis)
+{
 
-    for (int i = 0; i < y_axis; ++i) {
-        for (int j = 0; j <x_axis; ++j) {
+    for (int i = 0; i < y_axis; ++i) 
+    {
+        for (int j = 0; j <x_axis; ++j) 
+        {
             map[i][j]=1;
         }
     }
 }
 
 //void printMap(int x_axis,int y_axis) BY MinOS
-void printMap(int x_axis,int y_axis){
+void printMap(int x_axis,int y_axis)
+{
     printf("y\\x");
-    for (int i = 0; i < x_axis; ++i) {
+    for (int i = 0; i < x_axis; ++i) 
+    {
         printf("%3d",i);
     }
     printf("\n");
-    for (int i = 0; i < y_axis; ++i) {
+    for (int i = 0; i < y_axis; ++i)
+    {
         printf("%3d",i);
-        for (int j = 0; j <x_axis ; ++j) {
+        for (int j = 0; j <x_axis ; ++j) 
+        {
             char outChar;
-            if(map[i][j]==1){
+            if(map[i][j]==1)
+            {
                 outChar='*';
             }
-            else{
+            else
+            {
                 outChar=' ';
             }
             printf("  %c",outChar);
@@ -36,12 +48,30 @@ void printMap(int x_axis,int y_axis){
         printf("\n");
     }
 }
+void MineApper(int kick)//by lawliet
+{
+    int mine = 0 ;
+    int num = 0;//还需要重新赋值：显示周围雷数
+    if (kick == mine)
+    {
+        cout << "游戏结束" <<endl;
+    }
+    else 
+    {
+        cout << num <<endl;
+    } 
+
+}
 
 
-int main() {
+int main() 
+{
+    srand (time(NULL));
+    int kick = 0;//点击数
+
     std::cout << "Hello, World!" << std::endl;
 
-    scanf("%d %d",&x,&y);
+    scanf("%d %d",&x,&y);//为什么不用宏函数
 
     creatMap(x,y);
     printMap(x,y);
